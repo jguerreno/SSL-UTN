0f164969a207d0ad4fdc2a321eed472ec649ad7a
AlejoLovallo <alejoe.lovallo@gmail.com>
2021-07-18 23:53:13 -0300

    linked list

--- 03-Flex-Reconocimiento-Categorias-Lexicas/lib.c
+++ 03-Flex-Reconocimiento-Categorias-Lexicas/lib.c
@@ -1,3 +1,205 @@
 #include "lib.h"
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+/* Given a reference (pointer to pointer) to the head of a list
+   and an int,  inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+  
+    /* 2. put in the data  */
+    new_node->data  = new_data;
+  
+    /* 3. Make next of new node as head */
+    new_node->next = (*head_ref);
+  
+    /* 4. move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+
+/* Given a node prev_node, insert a new node after the given
+prev_node */
+void insertAfter(struct Node* prev_node, int new_data)
+{
+    /*1. check if the given prev_node is NULL */
+    if (prev_node == NULL)
+    {
+    printf("the given previous node cannot be NULL");    
+    return;
+    }
+         
+    /* 2. allocate new node */
+    struct Node* new_node =(struct Node*) malloc(sizeof(struct Node));
+ 
+    /* 3. put in the data */
+    new_node->data = new_data;
+ 
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;
+ 
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node;
+}
+
+
+/* Given a reference (pointer to pointer) to the head
+   of a list and an int, appends a new node at the end  */
+void append(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+ 
+    struct Node *last = *head_ref;  /* used in step 5*/
+  
+    /* 2. put in the data  */
+    new_node->data  = new_data;
+ 
+    /* 3. This new node is going to be the last node, so make next
+          of it as NULL*/
+    new_node->next = NULL;
+ 
+    /* 4. If the Linked List is empty, then make the new node as head */
+    if (*head_ref == NULL)
+    {
+       *head_ref = new_node;
+       return;
+    } 
+      
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)
+        last = last->next;
+  
+    /* 6. Change the next of last node */
+    last->next = new_node;
+    return;   
+}
+
+// This function prints contents of linked list starting from head
+void printList(struct Node *node)
+{
+  while (node != NULL)
+  {
+     printf(" %d ", node->data);
+     node = node->next;
+  }
+}
+
+
+/* Given a reference (pointer to pointer) to the head of a
+   list and a key, deletes the first occurrence of key in
+   linked list */
+void deleteNode(struct Node** head_ref, int key)
+{
+    // Store head node
+    struct Node *temp = *head_ref, *prev;
+ 
+    // If head node itself holds the key to be deleted
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next; // Changed head
+        free(temp); // free old head
+        return;
+    }
+ 
+    // Search for the key to be deleted, keep track of the
+    // previous node as we need to change 'prev->next'
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+ 
+    // If key was not present in linked list
+    if (temp == NULL)
+        return;
+ 
+    // Unlink the node from linked list
+    prev->next = temp->next;
+ 
+    free(temp); // Free memory
+}
+
+/* Given a reference (pointer to pointer) to the head of a list
+   and a position, deletes the node at the given position */
+void deleteNodeAt(struct Node **head_ref, int position)
+{
+   // If linked list is empty
+   if (*head_ref == NULL)
+      return;
+ 
+   // Store head node
+   struct Node* temp = *head_ref;
+ 
+    // If head needs to be removed
+    if (position == 0)
+    {
+        *head_ref = temp->next;   // Change head
+        free(temp);               // free old head
+        return;
+    }
+ 
+    // Find previous node of the node to be deleted
+    for (int i=0; temp!=NULL && i<position-1; i++)
+         temp = temp->next;
+ 
+    // If position is more than number of nodes
+    if (temp == NULL || temp->next == NULL)
+         return;
+ 
+    // Node temp->next is the node to be deleted
+    // Store pointer to the next of node to be deleted
+    struct Node *next = temp->next->next;
+ 
+    // Unlink the node from linked list
+    free(temp->next);  // Free memory
+ 
+    temp->next = next;  // Unlink the deleted node from list
+}
+
+/* Function to delete the entire linked list */
+void deleteList(struct Node** head_ref)
+{
+   /* deref head_ref to get the real head */
+   struct Node* current = *head_ref;
+   struct Node* next;
+ 
+   while (current != NULL)
+   {
+       next = current->next;
+       free(current);
+       current = next;
+   }
+   
+   /* deref head_ref to affect the real head back
+      in the caller. */
+   *head_ref = NULL;
+}
+
+/* Counts no. of nodes in linked list */
+int getListLength(struct Node* head)
+{
+    int count = 0;  // Initialize count
+    struct Node* current = head;  // Initialize current
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+
+/* Checks whether the value x is present in linked list */
+int search(struct Node* head, int x)
+{
+    struct Node* current = head;  // Initialize current
+    while (current != NULL)
+    {
+        if (current->data == x)
+            return 1;
+        current = current->next;
+    }
+    return 0;
+}
--- 03-Flex-Reconocimiento-Categorias-Lexicas/lib.h
+++ 03-Flex-Reconocimiento-Categorias-Lexicas/lib.h
@@ -5,4 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+void push(struct Node** head_ref, int new_data);
+void push(struct Node** head_ref, int new_data);
+void insertAfter(struct Node* prev_node, int new_data);
+void append(struct Node** head_ref, int new_data);
+void printList(struct Node *node);
+void deleteNode(struct Node** head_ref, int key);
+void deleteList(struct Node** head_ref);
+void deleteNodeAt(struct Node **head_ref, int position);
+int getListLength(struct Node* head);
+int search(struct Node* head, int x);
 #endif  // NUESTRA_LIB_
