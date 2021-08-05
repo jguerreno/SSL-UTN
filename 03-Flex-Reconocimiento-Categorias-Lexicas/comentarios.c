38a6ef7997f27fc6b88324a06fc061614c16f86f
cynthiaabbate <cabbate@frba.utn.edu.ar>
2021-08-05 18:47:29 -0300

    comentarios

--- 03-Flex-Reconocimiento-Categorias-Lexicas/comentarios.c
+++ 03-Flex-Reconocimiento-Categorias-Lexicas/comentarios.c
@@ -5,7 +5,6 @@
 #include <stdbool.h>
 
 
-
 void push(ComentarioNode** head, char comentario[]){
     /* 1. allocate node */
     ComentarioNode* new_node = (ComentarioNode*) malloc(sizeof(ComentarioNode));
@@ -72,8 +71,43 @@ void deleteList(ComentarioNode** head){
 
 
 void sortIdentidier(ComentarioNode** head){
+
 }
 
+void sort(ComentarioNode **head)
+{
+    ComentarioNode *aux, *aux2, *temp;
+    
+    int mayor = 0;
+    
+    aux = (*head);
+    
+    aux2 = aux;
+    
+    while (aux->next != NULL)
+    {
+        while (aux2 != NULL)
+        {
+            if (aux2->data.comentario >= mayor)
+                mayor = aux2->data.comentario;
+        }
+        
+        aux2 = aux;
+        
+        while (aux2->next != NULL)
+        {
+            if ((aux2->next)->data.comentario == mayor)
+            {
+                temp = (aux2->next)->next;
+                (aux2->next)->next= aux;
+                aux->next = temp;
+            }
+            aux2 = aux2->next;
+            
+        }
+        aux = aux->next;
+    }
+}
 
 void reporteComentarios(ComentarioNode** head,char comentario[]){
     push(head,comentario);
