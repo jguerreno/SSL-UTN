f56392eccf756004e4d04211dd8a8eeef6e1a71a
AlejoLovallo <alejoe.lovallo@gmail.com>
2021-08-16 18:11:26 -0300

    feat(constantes hex): listo

---
+++ 03-Flex-Reconocimiento-Categorias-Lexicas/constantesHex.c
@@ -0,0 +1,65 @@
+#include "constantesHex.h"
+#include "lib.h"   
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void pushConstHex(ConstHexNode** head, char *hex);
+DataConstHex newDataConstHex(char *hex);
+void printConstantesHexadecimales(ConstHexNode* node);
+void deleteListaConstanteHexadecimales(ConstHexNode** head);
+
+
+void pushConstHex(ConstHexNode** head, char *hex){
+    /* 1. allocate node */
+    ConstHexNode* new_node = (ConstHexNode*) malloc(sizeof(ConstHexNode));
+
+    /* 2. put in the data  */
+    new_node->data  = newDataConstHex(hex);
+
+    /* 3. Make next of new node as head */
+    new_node->next = (*head);
+
+    /* 4. move the head to point to the new node */
+    (*head)    = new_node;
+}
+
+
+DataConstHex newDataConstHex(char *hex){
+    DataConstHex data;
+
+    strcpy(data.constante, hex);
+
+    return data;
+}
+
+void printConstantesHexadecimales(ConstHexNode *node){
+
+    printf("CONSTANTES HEXADECIMALES\n");
+
+    while (node != NULL){
+     printf("%s\n", node->data.constante);
+     printf("Valor Decimal: %d\n",hexToDec(node->data.constante));
+
+     node = node->next;
+  }
+
+}
+
+void deleteListaConstanteHexadecimales(ConstHexNode** head){
+    /* deref head_ref to get the real head */
+    ConstHexNode* current = *head;
+    ConstHexNode* next;
+ 
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+   
+    /* deref head to affect the real head back in the caller. */
+    *head = NULL;
+}
+
