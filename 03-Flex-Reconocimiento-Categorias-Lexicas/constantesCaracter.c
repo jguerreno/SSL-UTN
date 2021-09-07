d91dfbf3773ba77c8b0e7aa805343fee0bd9d048
jguerreno <jguerrenolopez@frba.utn.edu.ar>
2021-09-07 13:15:55 -0300

    fix: constante de caracter

--- 03-Flex-Reconocimiento-Categorias-Lexicas/constantesCaracter.c
+++ 03-Flex-Reconocimiento-Categorias-Lexicas/constantesCaracter.c
@@ -19,6 +19,33 @@ void pushConstCarac(ConstCaracNode** head, char constCarac){
     (*head)    = new_node;
 }
 
+void appendConstantesCaracter(ConstCaracNode** head_ref, char constCarac){
+    /* 1. allocate node */
+    ConstCaracNode* new_node = (ConstCaracNode*) malloc(sizeof(ConstCaracNode));
+    
+    ConstCaracNode *last = (*head_ref);  /* used in step 5*/
+  
+    /* 2. put in the data  */
+    new_node->data  = newDataConstCarac(constCarac);
+ 
+    /* 3. This new node is going to be the last node, so make next of it as NULL*/
+    new_node->next = NULL;
+    
+    /* 4. If the Linked List is empty, then make the new node as head */
+    if ((*head_ref) == NULL){
+        (*head_ref) = new_node;
+    }
+    else{
+        /* 5. Else traverse till the last node */
+        while (last->next != NULL){
+            last = last->next;
+        }
+        
+        /* 6. Change the next of last node */
+        last->next = new_node;
+    }
+}
+
 
 DataConstCarac newDataConstCarac(char constCarac){
     DataConstCarac data;
