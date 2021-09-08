f9eb24b7b0f64d31b183354204d9dc3708c4f74d
Alejo Lovallo <alejoe.lovallo@gmail.com>
2021-09-08 20:50:13 -0300

    merge lib palabras into one file

--- 03-Flex-Reconocimiento-Categorias-Lexicas/palabras.c
+++ 03-Flex-Reconocimiento-Categorias-Lexicas/palabras.c
@@ -1,11 +1,68 @@
-#include "palabrasNoReconocidas.h"
+#include "palabras.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+/**
+ * PALABRAS RESERVADAS
+ * **/
+void appendPalabraReservada(PalabrasReservadasNode** head_ref, char* palabraReservada){
+    /* 1. allocate node */
+    PalabrasReservadasNode* new_node = (PalabrasReservadasNode*) malloc(sizeof(PalabrasReservadasNode));
+
+    PalabrasReservadasNode *last = *head_ref;  /* used in step 5*/
+  
+    /* 2. put in the data  */
+    new_node->data = newDataPalabraReservada(palabraReservada);
+    
+    /* 3. Make next of new node as head */
+    new_node->next = (*head_ref);
+
+    /* 4. move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+
+DataPalabraReservada newDataPalabraReservada(char* palabraReservada){
+    DataPalabraReservada data;
+
+    data.palabraReservada = strdup(palabraReservada);
+
+    return data;
+}
 
 
+void printPalabrasReservadas(FILE *reporte, PalabrasReservadasNode* node){
+
+    fprintf(reporte,"------------- PALABRAS RESERVADAS -------------\n");
+
+    while (node != NULL){
+     fprintf(reporte,"%s\n", node->data.palabraReservada);
+
+     node = node->next;
+    }
+}
+
+
+void deletePalabrasReservadas(PalabrasReservadasNode** head){
+    /* deref head_ref to get the real head */
+    PalabrasReservadasNode* current = *head;
+    PalabrasReservadasNode* next;
+ 
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+   
+    /* deref head to affect the real head back in the caller. */
+    *head = NULL;
+}
+
+/**
+ * PALABRAS NO RECONOCIDAS
+ * **/
 void pushPalabraNoReconocida(NoReconocidoNode** head, char palabraNoReconocida[], int linea){
     /* 1. allocate node */
     NoReconocidoNode* new_node = (NoReconocidoNode*) malloc(sizeof(NoReconocidoNode));
---
+++ 03-Flex-Reconocimiento-Categorias-Lexicas/palabras.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * PALABRAS RESERVADAS
+ * **/
+typedef struct DataPalabraReservada{
+    char* palabraReservada;
+} DataPalabraReservada;
+
+typedef struct PalabrasReservadasNode{
+    DataPalabraReservada data;
+    struct PalabrasReservadasNode* next;
+} PalabrasReservadasNode;
+
+/**
+ * PALABRAS NO RECONOCIDAS
+ * **/
+typedef struct DataPalabraNoReconocida{
+    char* noReconocida;
+    int linea;
+} DataPalabraNoReconocida;
+
+typedef struct NoReconocidoNode{
+    DataPalabraNoReconocida data;
+    struct NoReconocidoNode* next;
+} NoReconocidoNode;
+
+
+
+/**
+ * FUNCIONES PALABRAS RESERVADAS
+ * **/
+void appendPalabraReservada(PalabrasReservadasNode** head_ref, char* palabraReservada);
+DataPalabraReservada newDataPalabraReservada(char* palabraReservada);
+void printPalabrasReservadas(FILE *reporte, PalabrasReservadasNode* node);
+void deletePalabrasReservadas(PalabrasReservadasNode** head);
+
+
+/**
+ * FUNCIONES PALABRAS NO RECONOCIDAS
+ * **/
+void pushPalabraNoReconocida(NoReconocidoNode** head, char* palabraNoReconocida, int linea);
+DataPalabraNoReconocida newDataPalabraNoReconocida(char* palabraNoReconocida, int linea);
+void printPalabrasNoReconocidas(FILE *reporte, NoReconocidoNode* node);
+void deleteListaPalabrasNoReconocidas(NoReconocidoNode** head);
