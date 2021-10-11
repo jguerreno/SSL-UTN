b49c2e1e9d23665bce9536df21f84689ec605d03
jguerreno <jguerrenolopez@frba.utn.edu.ar>
2021-10-11 18:03:43 -0300

    Se agrego libreria perdida

--- 04-Bison-Reconocimiento_de_estructuras_de_frase/estructuraInvalida.c
+++ 04-Bison-Reconocimiento_de_estructuras_de_frase/estructuraInvalida.c
@@ -64,3 +64,59 @@ void printListEstructuraInvalida(FILE *reporte, EstructuraInvalidaNode* node){
   }
 
 }
+
+
+/****************************************************************************/
+void addErrorLexico(EstructuraErrorLexico** head, char* error){
+
+    EstructuraErrorLexico* node = searchErrorLexico(*head, error);
+
+    if(node == NULL){
+        pushErrorLexico(head, error);
+    }
+}
+
+void pushErrorLexico(EstructuraErrorLexico** head, char* error){
+    /* 1. allocate node */
+    EstructuraErrorLexico* new_node = (EstructuraErrorLexico*) malloc(sizeof(EstructuraErrorLexico));
+
+    /* 2. put in the data  */
+    new_node->data  = newDataErrorLexico(error);
+
+    /* 3. Make next of new node as head */
+    new_node->next = (*head);
+
+    /* 4. move the head to point to the new node */
+    (*head)    = new_node;
+}
+
+DataErrorLexico newDataErrorLexico(char* error){
+    DataErrorLexico data;
+
+    data.error= strdup(error);
+
+    return data;
+}
+
+EstructuraErrorLexico* searchErrorLexico(EstructuraErrorLexico* head, char* error){
+    EstructuraErrorLexico* current = head;  // Initialize current
+
+    while (current != NULL && strcmp(current->data.error, error)!=0){
+        current = current->next;
+    }
+
+   return current;
+}
+
+void printListErrorLexico(FILE *reporte, EstructuraErrorLexico* node){
+    
+    fprintf(reporte,"-------------------- ESTRUCTURAS INVALIDAS --------------------\n");
+    fprintf(reporte,"Estructura\t \t \t \n");
+
+
+    while (node != NULL){
+     fprintf(reporte,"%s\t \t \t \t \t \t \t\n", node->data.error);
+
+     node = node->next;
+    }
+}
