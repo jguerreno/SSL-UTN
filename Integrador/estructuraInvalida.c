dc36fe5fe6f39badb80377a7a5ae7f40a6e4a25f
jguerreno <jguerrenolopez@frba.utn.edu.ar>
2021-10-20 16:20:01 -0300

    feat: se agrego errores semanticos

---
+++ Integrador/estructuraInvalida.c
@@ -0,0 +1,134 @@
+#include "estructuraInvalida.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+void pushEstructuraInvalida(EstructuraInvalidaNode** head, int linea) {
+    /* 1. allocate node */
+    EstructuraInvalidaNode* new_node = (EstructuraInvalidaNode*) malloc(sizeof(EstructuraInvalidaNode));
+
+    /* 2. put in the data  */
+    new_node->data  = newDataEstructuraInvalida(linea);
+
+    /* 3. Make next of new node as head */
+    new_node->next = (*head);
+
+    /* 4. move the head to point to the new node */
+    (*head)    = new_node;
+}
+
+
+DataEstructuraInvalida newDataEstructuraInvalida(int line){
+    DataEstructuraInvalida data = {line};
+    return data;
+}
+
+
+void printListEstructuraInvalida(FILE *reporte, EstructuraInvalidaNode* node){
+
+    fprintf(reporte,"-------------------- ESTRUCTURAS INVALIDAS --------------------\n");
+
+    while (node != NULL){
+     fprintf(reporte,"Linea: \t \t \t \t \t \t %d\n", node->data.linea);
+
+     node = node->next;
+  }
+
+}
+
+
+/****************************************************************************/
+
+
+void addErrorLexico(EstructuraErrorLexico** head, char* error){
+
+    EstructuraErrorLexico* node = searchErrorLexico(*head, error);
+
+    if(node == NULL){
+        pushErrorLexico(head, error);
+    }
+}
+
+void pushErrorLexico(EstructuraErrorLexico** head, char* error){
+    /* 1. allocate node */
+    EstructuraErrorLexico* new_node = (EstructuraErrorLexico*) malloc(sizeof(EstructuraErrorLexico));
+
+    /* 2. put in the data  */
+    new_node->data  = newDataErrorLexico(error);
+
+    /* 3. Make next of new node as head */
+    new_node->next = (*head);
+
+    /* 4. move the head to point to the new node */
+    (*head)    = new_node;
+}
+
+DataErrorLexico newDataErrorLexico(char* error){
+    DataErrorLexico data;
+
+    data.error= strdup(error);
+
+    return data;
+}
+
+EstructuraErrorLexico* searchErrorLexico(EstructuraErrorLexico* head, char* error){
+    EstructuraErrorLexico* current = head;  // Initialize current
+
+    while (current != NULL && strcmp(current->data.error, error)!=0){
+        current = current->next;
+    }
+
+   return current;
+}
+
+void printListErrorLexico(FILE *reporte, EstructuraErrorLexico* node){
+    
+    fprintf(reporte,"-------------------- ESTRUCTURAS INVALIDAS --------------------\n");
+    fprintf(reporte,"Estructura\t \t \t \n");
+
+
+    while (node != NULL){
+     fprintf(reporte,"%s\t \t \t \t \t \t \t\n", node->data.error);
+
+     node = node->next;
+    }
+}
+
+
+/***************************** Error Semantico ***********************************************/
+
+void pushErrorSemantico(EstructuraErrorSemantico** head, char* error) {
+    /* 1. allocate node */
+    EstructuraErrorSemantico* new_node = (EstructuraErrorSemantico*) malloc(sizeof(EstructuraErrorSemantico));
+
+    /* 2. put in the data  */
+    new_node->data = newDataErrorSemantico(error);
+
+    /* 3. Make next of new node as head */
+    new_node->next = (*head);
+
+    /* 4. move the head to point to the new node */
+    (*head)    = new_node;
+}
+
+
+DataErrorSemantico newDataErrorSemantico(char* error) {
+    DataErrorSemantico data;
+    data.error = strdup(error);
+    return data;
+}
+
+
+void printListErrorSemantico(FILE *reporte, EstructuraErrorSemantico* node) {
+
+    fprintf(reporte,"-------------------- Error Semanticos --------------------\n");
+
+    while (node != NULL){
+        fprintf(reporte,"Error: \t \t %s\n", node->data.error);
+
+        node = node->next;
+    }
+}
+
---
+++ Integrador/estructuraInvalida.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+typedef struct DataEstructuraInvalida{
+    int linea;
+} DataEstructuraInvalida;
+
+typedef struct EstructuraInvalidaNode{
+    DataEstructuraInvalida data;
+    struct EstructuraInvalidaNode* next;
+} EstructuraInvalidaNode;
+
+
+void pushEstructuraInvalida(EstructuraInvalidaNode** head,int linea);
+DataEstructuraInvalida newDataEstructuraInvalida(int linea);
+void printListEstructuraInvalida(FILE *reporte, EstructuraInvalidaNode* node);
+
+/****************************************************************************/
+
+typedef struct DataErrorLexico{
+    char* error;
+} DataErrorLexico;
+
+typedef struct EstructuraErrorLexico{
+    DataErrorLexico data;
+    struct EstructuraErrorLexico* next;
+} EstructuraErrorLexico;
+
+void addErrorLexico(EstructuraErrorLexico** head, char* error);
+void pushErrorLexico(EstructuraErrorLexico** head, char* error);
+DataErrorLexico newDataErrorLexico(char* error);
+EstructuraErrorLexico* searchErrorLexico(EstructuraErrorLexico* head, char* error);
+void printListErrorLexico(FILE *reporte, EstructuraErrorLexico* node);
+
+/****************************************************************************/
+
+typedef struct DataErrorSemantico {
+    char* error;
+} DataErrorSemantico;
+
+typedef struct EstructuraErrorSemantico {
+    DataErrorSemantico data;
+    struct EstructuraErrorSemantico* next;
+} EstructuraErrorSemantico;
+
+
+void pushErrorSemantico(EstructuraErrorSemantico** head, char* error);
+DataErrorSemantico newDataErrorSemantico(char* error);
+void printListErrorSemantico(FILE *reporte, EstructuraErrorSemantico* node);
